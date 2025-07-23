c79b74083a0a595488758bb61388d9fc96719776
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-23 18:48:58 +0530

    conversation betwwen ramesh and nagendra

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#conversation between ramesh and nagendra.
+
+echo "nagendra:: Hi ramesh, how are you"
+echo "ramesh:: Hi nagendra, Iam fine, how about you"
+echo "nagendra:: I ma doing good ramesh ,whats going on"
+echo :ramesh:: I am learning shell script"
