89bced59e47340794149f8ef15537a97cde57e9c
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-23 20:13:02 +0530

    variables

---
+++ 05-read-command.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "please enter username"
+
+read -s USERNAME #takes input into USERNAME as variable
+
+echo "USERNAME entered is : $USERNAME"
+
+echo "please enter password:"
+
+read -s PASSWORD
+
+echo "password entered is: $PASSWORD"
+
+
