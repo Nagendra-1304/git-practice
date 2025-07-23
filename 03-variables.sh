9e7c28ba6d07681ae08760069a56822e7c8d0fb4
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-23 19:32:00 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,7 @@
 $PERSON1=nagendra
 $PERSON2=ramesh
 
-echo "$PERSON1:: Hi $PERSON2, how are you"
-echo "$PERSON2:: Hi $PERSON1, Iam fine, how about you"
-echo "$PERSON1:: I ma doing good $PERSON2 ,whats going on"
-echo "$PERSON2:: I am learning shell script"
+echo "$PERSON1:: Hi ${PERSON2}, how are you"
+echo "${PERSON2}:: Hi $PERSON1, Iam fine, how about you"
+echo "$PERSON1:: I ma doing good ${PERSON2} ,whats going on"
+echo "${PERSON2}:: I am learning shell script"
