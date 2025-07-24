bb5a91a45aa774e93b8f2439cdab36cd30757e3a
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-24 13:17:33 +0530

    command-output

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "user id is: $USERID"
+#echo "user id is: $USERID"
+
+if [ $USERID -ne 0 ] 
+then 
+   echo "please run this scriot with root privilages"
+
+fi
+dnf install git -y
