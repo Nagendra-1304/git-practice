54cce96f8b735ba827a5e640a95197ae2f47bfbb
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-24 13:32:48 +0530

    command-output

--- 11-installation.sh
+++ 11-installation.sh
@@ -8,5 +8,11 @@ then
    echo "please run this scriot with root privilages"
    exit 1
 fi
-dnf install git -y
-
+dnf  list install git 
+if [ $? -ne 0 ]
+then 
+   echo "git is not installed, going to install it..."
+   dnf install git -y
+else
+    echo "git is already installed, nothing to do.."   
+fi
