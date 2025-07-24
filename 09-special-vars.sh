2c5f42824cb381572b5787422614b1cb9fc8e48d
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-24 12:23:24 +0530

    command-output

---
+++ 09-special-vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "all variable passed to the script: $@"
+echo "Number of variables passed: $#"
+echo "script name $0"
+echo "current working directory: $PWD"
+echo "Home directory of present user: $HOME"
+echo "PID of the script executing now: $$"
