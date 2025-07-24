77b51d355bf7974d907240d07dc1dc726045b08d
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-24 13:40:24 +0530

    command-output

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,14 +5,21 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ] 
 then 
-   echo "please run this scriot with root privilages"
+   echo "please run this script with root privilages"
    exit 1
 fi
 dnf  list install git 
 if [ $? -ne 0 ]
 then 
    echo "git is not installed, going to install it..."
-   dnf install git -y
+   dnf install gittt -y
+   if [ $? -ne 0 ]
+   then 
+       echo "installation is not success, check it.."
+       exit 1
+   else
+     echo "installation is success"
+   fi
 else
     echo "git is already installed, nothing to do.."   
 fi
