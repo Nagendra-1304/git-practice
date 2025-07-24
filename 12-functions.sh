b213b5a238eb182c014f10b447f1d5cc34338a97
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-24 15:05:50 +0530

    command-output

--- 12-functions.sh
+++ 12-functions.sh
@@ -18,34 +18,22 @@ then
    exit 1
 fi
 dnf  list install git
-VALIDATE $? "Listing Git"
-# if [ $? -ne 0 ]
-# then 
-#    echo "git is not installed, going to install it..."
-#    dnf install git -y
-#    if [ $? -ne 0 ]
-#    then 
-#        echo "installation is not success, check it.."
-#        exit 1
-#    else
-#      echo "installation is success"
-#    fi
-# else
-#     echo "git is already installed, nothing to do.."   
-# fi
 
-# dnf list install mysql
-# if [ $? -ne 0 ]
-# then 
-#     echo " mySQL is not installed, going to install"
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "mySQL installation is failed, please check it.."
-#         exit 1
-#     else
-#         echo "installation success"
-#     fi
-# else 
-#     echo "mysql is already installed, nothing to do..."
-# fi
+if [ $? -ne 0 ]
+then 
+   echo "git is not installed, going to install it..."
+   dnf install git -y
+   VALIDATE $? "installing Git"
+else
+    echo "git is already installed, nothing to do.."   
+fi
+
+dnf list install mysql
+if [ $? -ne 0 ]
+then 
+    echo " mySQL is not installed, going to install"
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+else 
+    echo "mysql is already installed, nothing to do..."
+fi
