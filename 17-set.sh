1a765af50162be0741225a638cfa84e6cd47f390
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-25 14:51:07 +0530

    Expense-shell

--- 17-set.sh
+++ 17-set.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "hello world"
-echooooo "Hello world"
-echo " Hello world"
+set -e #setting the automatic exit, if we get errror, set e-ex for debug
+echooooo "Hello world failure"
+echo " Hello world after failure"
