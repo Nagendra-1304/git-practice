d2fd01dd0804b2dba16f360f433c3372dd8800a9
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-25 14:56:36 +0530

    Expense-shell

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 echo "hello world"
-set -e #setting the automatic exit, if we get errror, set e-ex for debug
+set -e #setting the automatic exit, if we get errror, set -ex for debug
+
+
+failure()
+{
+    echo "Failed at: $1:$2" 
+}
+
+trap 'failuere "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
 echooooo "Hello world failure"
 echo " Hello world after failure"
